ef093efde0eb270b4933f6cce4711bfa68e0f4df
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2022-11-22 14:17:03 +0300

    fix

--- src/components/MainLending/AboutMe/data.js
+++ src/components/MainLending/AboutMe/data.js
@@ -1,10 +1,10 @@
 export const socialLinks = [
   { id: 1, name: "Telegram", link: "https://t.me/apl_by" },
-  { id: 2, name: "Github", link: "https://github.com/Apl-by" },
+  { id: 2, name: "Github", link: "https://github.com/apl-by" },
 ];
 
 export const infoAbout = {
   name: "Александр",
-  description: "Веб-разработчик, 33 года",
+  description: "Веб-разработчик, 35 лет",
   about: "Бла-бла-бла, котик на фото #милота #уменялапки #возьмитекотанаработу",
 };
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export const BASE_URL_MOVIES_API = "https://api.nomoreparties.co";
-export const BASE_URL_MAIN_API = "https://api.apl-by.site";
+export const BASE_URL_MAIN_API = "https://api.diploma.apl-by.ru";
 // export const BASE_URL_MAIN_API = "http://localhost:3000";
 
 export const ERR_API =
